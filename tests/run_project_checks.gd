931f4cb904e0b8cc067654a869340aba4ce90bb0
Afc_Adinho <afcadinho@gmail.com>
2026-08-19 23:18:36 +0200

    Honor fixed Senior Staff permissions in UI

--- tests/run_project_checks.gd
+++ tests/run_project_checks.gd
@@ -66,6 +66,7 @@ const CHECK_SCRIPTS: Array[String] = [
 	"res://tests/account_email_login_check.gd",
 	"res://tests/privacy_account_controls_check.gd",
 	"res://tests/client_version_contract_check.gd",
+	"res://tests/senior_staff_permission_projection_check.gd",
 	"res://tests/localization_launcher_news_check.gd",
 	"res://tests/localization_market_ui_check.gd",
 	"res://tests/localization_pokedex_ui_check.gd",
