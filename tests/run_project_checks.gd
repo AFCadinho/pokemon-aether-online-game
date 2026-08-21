c3af6c31efad31f964b36872c0da0efc5c0c1d2a
Afc_Adinho <afcadinho@gmail.com>
2026-08-21 06:16:08 +0200

    Reduce NPC dialogue startup latency

--- tests/run_project_checks.gd
+++ tests/run_project_checks.gd
@@ -202,6 +202,7 @@ const CHECK_SCRIPTS: Array[String] = [
 	"res://tests/npc_dialogue_resolver_check.gd",
 	"res://tests/dialogue_metadata_service_check.gd",
 	"res://tests/dialogue_input_lock_check.gd",
+	"res://tests/npc_dialogue_latency_check.gd",
 	"res://tests/trainer_dialogue_cleanup_check.gd",
 	"res://tests/npc_content_validation_check.gd",
 	"res://tests/item_gift_npc_check.gd",
