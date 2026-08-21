69c3cfa3ef1e34791ecce7d5ee093698d5e74d32
Afc_Adinho <afcadinho@gmail.com>
2026-08-21 06:39:20 +0200

    Add Mega Champions catalog projection

--- tests/run_project_checks.gd
+++ tests/run_project_checks.gd
@@ -27,6 +27,7 @@ const CHECK_SCRIPTS: Array[String] = [
 	"res://tests/battle_state_deferred_hp_check.gd",
 	"res://tests/battle_state_hp_cursor_check.gd",
 	"res://tests/battle_state_primal_forms_check.gd",
+	"res://tests/mega_champions_catalog_check.gd",
 	"res://tests/battle_mechanical_suspension_state_check.gd",
 	"res://tests/battle_timer_projection_check.gd",
 	"res://tests/battle_timer_ui_visibility_check.gd",
