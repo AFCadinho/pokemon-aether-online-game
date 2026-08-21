01743f84852f938516eedfe56bea0b39caffd213
Afc_Adinho <afcadinho@gmail.com>
2026-08-21 11:05:40 +0200

    Mark horizontal stairs across outdoor maps

--- tests/run_project_checks.gd
+++ tests/run_project_checks.gd
@@ -123,6 +123,7 @@ const CHECK_SCRIPTS: Array[String] = [
 	"res://tests/map_music_profile_check.gd",
 	"res://tests/map_layer_resolver_check.gd",
 	"res://tests/horizontal_stair_elevation_check.gd",
+	"res://tests/outdoor_horizontal_stair_markers_check.gd",
 	"res://tests/pokemon_center_template_structure_check.gd",
 	"res://tests/aether_atelier_check.gd",
 	"res://tests/aether_atelier_runtime_check.gd",
