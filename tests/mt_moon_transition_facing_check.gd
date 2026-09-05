58a3a804187db504afc51a5f64ebecbdbc3bd237
Afc_Adinho <afcadinho@gmail.com>
2026-09-05 06:33:32 +0200

    fix: correct Mt. Moon B2F arrival facing

--- tests/mt_moon_transition_facing_check.gd
+++ tests/mt_moon_transition_facing_check.gd
@@ -16,7 +16,7 @@ const EXPECTED_FACING := {
 	"kanto_mt_moon__to_route_4": "down",
 	"kanto_route_4__to_cerulean_city": "right",
 	"kanto_cerulean_city__to_route_4": "left",
-	"kanto_mt_moon_b2f__to_b1f_2": "left",
+	"kanto_mt_moon_b2f__to_b1f_2": "right",
 	"kanto_mt_moon_b2f__to_b1f_4": "left",
 	"kanto_mt_moon_b2f__to_b1f_6": "right",
 	"kanto_mt_moon_b2f__to_b1f_7": "right",
